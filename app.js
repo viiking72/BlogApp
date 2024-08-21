fa31a1aa5e194ac63700729abfa8652dec7c41fd
viking72 <100124541+viiking72@users.noreply.github.com>
2024-08-21 16:48:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const cors=require('cors');
 
 const connectDB = require('./server/config/db');
 const { isActiveRoute } = require('./server/helpers/routeHelpers');
@@ -20,6 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(methodOverride('_method'));
+app.use(cors());
 
 app.use(session({
   secret: 'keyboard cat',
